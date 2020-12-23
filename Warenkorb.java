503631ce1074bf294c48fb25e00201bfd8c75b03
alexprummern <76532996+alexprummern@users.noreply.github.com>
2020-12-23 20:29:07 +0100

    Update Warenkorb.java

--- Warenkorb.java
+++ Warenkorb.java
@@ -4,15 +4,22 @@ public class Warenkorb {
 
     //Attribute
 
+    private  int artikelmenge;
+    private int[][] inhaltWarenkorb;
 
 
     //Konstruktor
     public Warenkorb(){
+        inhaltWarenkorb=new int[100][2];
     }
 
     //Methoden
 
 
 
+    //setter
+    public void setArtikelmenge(int artikelmenge) {
+        this.artikelmenge = artikelmenge;
+    }
 
 }
